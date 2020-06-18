6edb7199cdcfe02686ac1a840116335c867d2350
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-18 20:00:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   
   def match(list_of_words)
     list_of_words.each do |thing|
-      if @word.split("").sort == thing.split("").sort
+      if @word.split("").sort == @word.split("").sort
         
         
     end 
