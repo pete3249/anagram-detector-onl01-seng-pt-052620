a1503b474929a375d0d5e7871b425563da62745e
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-18 20:05:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(list_of_words)
+    matching_anagrams = []
+    list_of_words.each do |thing|
+      if @word.split("").sort == thing.split("").sort
+        matching_anagrams << thing
+      end 
+    end 
+    matching_anagrams
+  end 
+  
+end 
